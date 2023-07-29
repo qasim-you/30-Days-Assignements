73bd7324af68d1a63da9d5056820e786de7f3a1c
Qasim <qasimyousaf094@gmail.com>
2023-07-29 23:34:19 +0500

    initial commit

--- New folder/day2.js
+++ New folder/day2.js
@@ -1,1 +1,15 @@
 // create counter function in js 
+function createValue(n) {
+    let counterValue = n ;
+    function counter() {
+        const currentValue = counterValue ;
+        counterValue += 1 ;
+        return currentValue;
+    } 
+    return counter;
+    
+}
+let result = createValue(5) ;
+console.log(result());
+console.log(result());
+console.log(result());
